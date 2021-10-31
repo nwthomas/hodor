f13d04d9579bfa444eb8868aaa6276ca49078ee6
Nathan Thomas <nwthomas@me.com>
2021-10-31 12:59:55 -0700

    Add code comments for functions

--- contracts/Hodor.sol
+++ contracts/Hodor.sol
@@ -5,6 +5,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
+/// @title A contract for hodling ether, ERC20, and ERC721 tokens for a predefined period of time
+/// @author Nathan Thomas
+/// @notice This contract is not audited - USE AT YOUR OWN RISK!
 contract Hodor is Ownable {
   // This is necessary to check if a given provided address supports the
   // ERC721 interface definition. It's pulled from:
@@ -23,12 +26,8 @@ contract Hodor is Ownable {
    *   bytes4(keccak256('safeTransferFrom(address,address,uint256,bytes)'))
    */
 
-  uint256 public unlockTime;
+  uint256 public unlockTimeSeconds;
   uint256 public totalEther;
-
-  mapping(address => uint256) public erc20TokensToAmounts;
-  mapping(address => bool) public erc721ToIndex;
-  uint256[] public erc721TokenIDs;
 
   event ReceiveEther(uint256 etherAmount);
   event TransferEther(address payableAddress, uint256 etherAmount);
@@ -44,7 +43,7 @@ contract Hodor is Ownable {
   );
 
   modifier isUnlocked() {
-    require(block.timestamp >= unlockTime, "Error: Contract is locked.");
+    require(block.timestamp >= unlockTimeSeconds, "Error: Contract is locked.");
     _;
   }
 
@@ -56,21 +55,29 @@ contract Hodor is Ownable {
     _;
   }
 
+  /// @notice Instantiates a new contract with a predefined period of time to lock the contract
+  /// for forced hodling
+  /// @param _timeToLockSeconds The amount of time in seconds to lock the contract
+  /// @dev Ether can be send to this contract on instantiation
   constructor(uint256 _timeToLockSeconds) payable {
     if (msg.value > 0) {
       totalEther += msg.value;
       emit ReceiveEther(msg.value);
     }
 
-    unlockTime = block.timestamp + _timeToLockSeconds;
+    unlockTimeSeconds = block.timestamp + _timeToLockSeconds;
   }
 
+  /// @notice Allows any address to contribute to the contract (regardless of ownership status)
   receive() external payable {
     uint256 newEther = msg.value;
     totalEther += newEther;
     emit ReceiveEther(msg.value);
   }
 
+  /// @notice Allows the owner to retrieve all ether from the contract if the forced hodling
+  /// period has been passed
+  /// @param _payableAddress The address that the ether should be sent to
   function retrieveEther(address _payableAddress)
     external
     payable
@@ -86,6 +93,11 @@ contract Hodor is Ownable {
     }
   }
 
+  /// @notice Allows the owner to retrieve all ERC20 tokens to a specified payable wallet
+  /// address from a specified token contract address if the forced hodling period has been
+  /// passed
+  /// @param _payableAddress The address that the ERC20 tokens should be sent to
+  /// @param _tokenAddress The ERC20 token contract address
   function retrieveERC20Tokens(address _payableAddress, address _tokenAddress)
     external
     onlyOwner
@@ -109,6 +121,12 @@ contract Hodor is Ownable {
     emit TransferERC20Token(_payableAddress, _tokenAddress, amount);
   }
 
+  /// @notice Allows the owner to send a specific ERC721 token to a specified payable wallet
+  /// address from a specified token contract address if the forced hodling period has been
+  /// passed
+  /// @param _payableAddress The address that the ERC721 token should be sent to
+  /// @param _tokenAddress The ERC721 token contract address
+  /// @param _tokenId The unique ID of the ERC721 token that should be sent to the _payableAddress
   function retriveERC721Token(
     address _payableAddress,
     address _tokenAddress,
@@ -124,6 +142,7 @@ contract Hodor is Ownable {
         )
       )
     );
+
     require(
       IERC721(_tokenAddress).ownerOf(_tokenId) == address(this),
       "Error: This contract does not own that ERC721 token."
